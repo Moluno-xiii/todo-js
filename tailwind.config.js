98b4ef468110ef11d1c864f6c872640932a60514
Moluno-xiii <molunoprogress@gmail.com>
2024-06-27 08:23:51 +0100

    initialized tailwind successfully

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        pantoneGreen: "#7C9670",
+        pantonePurple: "#76697E",
+        pantoneTangerine: "#DD4123",
+      },
+    },
+  },
+  plugins: [],
+};
