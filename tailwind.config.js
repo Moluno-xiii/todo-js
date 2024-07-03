ae6615827cb5f2b4667c6060f41912325bd10339
Moluno-xiii <molunoprogress@gmail.com>
2024-07-03 19:54:56 +0100

    ui done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
         pantoneGreen: "#7C9670",
         pantonePurple: "#76697E",
         pantoneTangerine: "#DD4123",
+        pantoneBlue: "#0953BC",
+        pantoneRed: "#AA0A24",
       },
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
